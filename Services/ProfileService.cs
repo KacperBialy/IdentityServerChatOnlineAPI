0af3614b7e624d0625c32dc2483ff917daa48c15
Kacper Biały <44371092+KacperBialy@users.noreply.github.com>
2022-02-07 13:35:42 +0100

    Delete unused claim

--- Services/ProfileService.cs
+++ Services/ProfileService.cs
@@ -25,7 +25,6 @@ namespace IdentityServer.Services
             var claims = new List<Claim>()
             {
                 new Claim("Email", user.Email),
-                new Claim("userName", user.UserName)
             };
 
             context.IssuedClaims.AddRange(claims);
